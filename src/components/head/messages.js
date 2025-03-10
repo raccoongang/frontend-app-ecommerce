a9755908382e9a5ecf8cfc4e772b78d56becbfd3
vladislavkeblysh <vladislav.keblysh@raccoongang.com>
2025-03-10 19:01:49 +0200

    feat: [Sigma-1764] add head component

---
+++ src/components/head/messages.js
@@ -0,0 +1,11 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  'orders.page.title': {
+    id: 'orders.page.title',
+    defaultMessage: 'Order History | {siteName}',
+    description: 'Title tag',
+  },
+});
+
+export default messages;
